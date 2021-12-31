f1134e65d0d4b1c7de2ad5b314e178133b58a99d
Renu Prajapati <prajapatirenu790@gmail.com>
2022-01-01 00:56:25 +0530

    Components Part 1

--- src/index.js
+++ src/index.js
@@ -4,8 +4,8 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import $ from 'jquery';
-import Popper from 'popper.js';
+//import $ from 'jquery';
+//import Popper from 'popper.js';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
 
 ReactDOM.render(
