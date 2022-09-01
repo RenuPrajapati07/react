9e394096f12eda1a475187cb07daf38cecbd5474
Renu Prajapati <prajapatirenu790@gmail.com>
2022-09-01 16:51:45 +0530

    React Router

---
+++ src/components/HomeComponent.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Container } from 'reactstrap';
+
+function Home(props) {
+    return (
+        <div className="Container">
+            <h4>Home</h4>
+        </div>
+    );
+}
+
+export default Home;
